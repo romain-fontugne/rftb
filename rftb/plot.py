d751e2233d412bc75dec6ca0e996b19981b869f3
romain.fontugne <romain.fontugne@gmail.com>
2019-01-16 15:49:43 +0900

    add an option to start from y=0

--- rftb/plot.py
+++ rftb/plot.py
@@ -1,23 +1,28 @@
 import numpy as np
 from matplotlib import pylab as plt
 
-def ecdf(a, ax=None, **kwargs):
+def ecdf(a, ax=None, add_zero=True, **kwargs):
     """Plot the Empirical Cumulative Density Function.
     
     parameters:
         a: The data of interest. Should be a list or a 1D numpy array.
         ax: The axes of the figure.
+        add_zero: Set to True to connect the graph to y=0
         **kwargs: Any other option to pass to pylab.plot.
         
     Returns a dictionary providing the CDF value for each point on the x axis.
     """
     sorted=np.sort( a )
     yvals=np.arange(len(sorted)+1)/float(len(sorted))
-    sorted = np.append( sorted[0], sorted )
-    if ax is None:
-        plt.plot( sorted, yvals, **kwargs )
+    if add_zero:
+        starti = 0
+        sorted = np.append( sorted[0], sorted) )
     else:
-        ax.plot( sorted, yvals, **kwargs )
+        starti=1
+    if ax is None:
+        plt.plot( sorted, yvals[starti:], **kwargs )
+    else:
+        ax.plot( sorted, yvals[starti:], **kwargs )
 
     return {k:v for k,v in zip(sorted, yvals)}
 
